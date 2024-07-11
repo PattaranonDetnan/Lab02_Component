d52484f89cc52ed02909bbc214bf1f9dc528e37b
pattaranon_detnanta <pattaranon_detnanta@cmu.ac.th>
2024-07-11 21:32:01 +0700

    15.3

---
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+import EventCard2 from '@/components/EventCard2.vue';
+import Event from '@/types/Event'
+import {ref,onMounted} from 'vue'
+import axios from 'axios'
+
+const events = ref<Event[]>(null)
+
+onMounted(()=>{
+  axios
+  .get('[https://my-json-server.typicode.com/PattaranonDetnan/MockServer/db]')
+  .then((response)=>{
+    console.log(response.data)
+  })
+  .catch((error)=>{
+    console.error('There was an error!',error)
+  })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+      <EventCard v-for="event in events" :key="event.id" :event="event"/>  
+      <EventCard2 v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  text-align: right;
+}
+
+</style>
