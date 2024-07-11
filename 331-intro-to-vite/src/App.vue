55820b3df57f20a79819fc72b7f666eef16e7ddb
pattaranon_detnanta <pattaranon_detnanta@cmu.ac.th>
2024-07-11 20:36:25 +0700

    8.7

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -1,43 +1,44 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-<div id="layout">
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink> |
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <RouterView/>
-</div>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-
-
-#layout{
-  font-family: Avenir,Arial, Helvetica, sans-serif;
+<style>
+#layout {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
   -webkit-font-smothing: antialsed;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-nav{
-  padding: 30px
+nav {
+  padding: 30px;
 }
 
 nav a {
   font-weight: bold;
-  color:#2c3e50;
+  color: #2c3e50;
 }
 
-nav a.router-link-exact-active{
+nav a.router-link-exact-active {
   color: #42b983;
 }
 
+h2 {
+  font-size: 20px;
+}
 </style>
