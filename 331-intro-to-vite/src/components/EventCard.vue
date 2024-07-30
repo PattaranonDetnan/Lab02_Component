7164a065b542ca96113f6529405d3c30ff391da1
pattaranon_detnanta <pattaranon_detnanta@cmu.ac.th>
2024-07-31 01:00:16 +0700

    2.9

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import Event from '@/types/Event'
+import Event from '@/types'
 
 defineProps<{
   event: Event
@@ -20,10 +20,11 @@ defineProps<{
 </script>
 
 <template>
-  <div class="event-card">
+  
+    <RouterLink class="event-link" :to="{name:'event-detail-view', params: {id:event.id} }">
     <h2>{{ event.title }}</h2>
     <span>@{{ event.time }} on {{ event.date }}</span>
-  </div>
+    </RouterLink>
 </template>
 
 <style scoped>
@@ -37,5 +38,9 @@ defineProps<{
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
